a0d426609a2793659b9928ac03e0d88f7fdc108c
Shashwat Mishra <smishra@solveda.com>
2023-08-22 22:11:03 +0530

    Small change in auth route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,45 +1,46 @@
-
 import connectToDB from "@/database";
 import User from "@/models/user";
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 
 const authOptions = {
-    providers:[
-        GoogleProvider({
-            clientId:"828540328008-ek2q99nivdph0ee1cvhnurb86gjsmg7r.apps.googleusercontent.com",
-            clientSecret: "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",
-        }),
-    ],
-    callbacks:{
-        async Signin({user, account}){
-            if(account.provider === "google"){
-                const {name, email} = user;
-                try{
-                    await connectToDB();
-                    const isUserExist = await User.findOne({email});
-                    
-                    if(!isUserExist){
-                        const res = await fetch("http://localhost:3000/api/user",{
-                            method: "POST",
-                            headers: {
-                                "Content-Type":"application/json",
-                            },
-                            body: JSON.stringify({name, email}),
-                        });
-                        if(res.success){
-                            return user;
-                        }
-                    }
-                } catch (error) {
-                    console.log(error);
-                }
+  providers:[
+    GoogleProvider({
+      clientId:"828540328008-ek2q99nivdph0ee1cvhnurb86gjsmg7r.apps.googleusercontent.com",
+      clientSecret: "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",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account.provider === "google") {
+        const { name, email } = user;
+        try {
+          await connectToDB();
+          const isUserExists = await User.findOne({ email });
+
+          if (!isUserExists) {
+            const res = await fetch("http://localhost:3001/api/user", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ name, email }),
+            });
+
+            if (res.success) {
+              return user;
             }
-            return user
-        },
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      return user;
     },
+  },
 };
 
 const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
